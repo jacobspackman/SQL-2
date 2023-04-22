83ae62038db163e316aa3cda8a19127c24dba5b8
Jacob Spackman <jacobts202@gmail.com>
2023-04-22 11:27:49 -0600

    complete

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
+const { CONNECTION_STRING } = process.env;
+
 const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
     dialect: 'postgres',
     dialectOptions: {
@@ -29,7 +31,7 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
+        sequelize.query(`UPDATE cc_appointments SET approved = true WHERE appt_id = ${apptId}
         
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
@@ -40,5 +42,31 @@ module.exports = {
                 nextEmp += 2
             })
             .catch(err => console.log(err))
+    },
+
+    getAllClients: (req, res) => {
+        sequelize.query(`SELECT * FROM cc_users JOIN cc_clients ON cc_users.user_id = cc_clients.user_id
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+    },
+
+    getPendingAppointments: (req, res) => {
+        sequelize.query(`SELECT * FROM cc_appointments WHERE approved = false ORDER BY date`).then(dbRes => res.status(200).send(dbRes[0]))
+    },
+
+    getPastAppointments: (req, res) => {
+        sequelize.query(`SELECT a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        WHERE a.approved = true AND a.completed = true
+        ORDER BY date DESC
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+    },
+
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+
+        sequelize.query(`UPDATE cc_appointments SET completed = true WHERE appt_id = ${apptId}`).then(dbRes => res.status(200).send(dbRes[0]))
     }
 }
